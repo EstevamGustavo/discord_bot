f8780d144f40054c90320a77a511e4317970fda3
Gustavo Estevam <gustavo.estevam@outlook.com>
2020-08-16 04:56:37 -0300

    init!!

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { config } = require('dotenv')
+
+config({
+    path: __dirname + "/.env"
+})
+
+client.on("ready", () => {
+    console.log("Bot is Ready!")
+})
+
+client.on("message", message => {
+    const prefix = ">roll"
+
+    if (message.content.startsWith(prefix)) {
+        const list = message.content.replace(prefix, "").split(/\n/)
+        let currentIndex = list.length
+
+        while (currentIndex !== 0) {
+            let index = Math.floor(Math.random() * currentIndex)
+            currentIndex -= 1
+
+            let aux = list[currentIndex]
+            list[currentIndex] = list[index]
+            list[index] = aux
+
+        }
+        const team1 = list.splice(0, list.length / 2)
+        const team2 = list.splice((list.length / 2) - 2, list.length)
+        message.reply(`\nTeam1 => ${team1} \nTeam2 => ${team2}`)
+    }
+})
+
+client.login(process.env.TOKEN);
